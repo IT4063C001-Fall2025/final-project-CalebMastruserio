961bcca347fcdb1298c22ce34a69f28210023923
CalebMastruserio <mastrucb@mail.uc.edu>
2025-09-27 22:50:53 -0400

    Converted the Notebook file.

--- source.py
+++ source.py
@@ -1,37 +1,48 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # {Project Title}📝
+# # Right to Repair Movement Analysis📝
 # 
 # ![Banner](./assets/banner.jpeg)
 
 # ## Topic
 # *What problem are you (or your stakeholder) trying to address?*
-# 📝 <!-- Answer Below -->
+# 
+# The social issue I am trying to address is the the Right to Repair Movement. The growing probelm at hand is in the modern construction and design of new vehciles today, being made so the average mechanic or even DIYer cannot work on or repair their own car without taking it to the manufacturer for speaclized work. By is a growing problem in the modern automotive industry as newer cars become more and more complex and complicated to work on and repair. With modules upon modules to service and scan that require dealer-only diagnostic equipment and software, this prevents many car owners from DIYers to small independent shops from servicing and working on their cars without taking them to the expensive dealership. 
 
 # ## Project Question
 # *What specific question are you seeking to answer with this project?*
 # *This is not the same as the questions you ask to limit the scope of the project.*
-# 📝 <!-- Answer Below -->
+# 
+# The question I am seeking to answer is how much more expensive are dealerships in conjucnction with the growing requirement of proprietory software in cars compare to in cost of independent shops or even DIYers and what role can the Right to Repair Movement play in reducing those costs?
 
 # ## What would an answer look like?
 # *What is your hypothesized answer to your question?*
-# 📝 <!-- Answer Below -->
+# 
+# The average cost of modern vehicles and repairs of newer cars have increase significantly. With the technological shift from mechanically driven, simple, and easy to work on cars that were once software dependent, have raised modern day repair costs and resticted consumer choice. The right to repair movement offers a solution to the growing complexity of modern construction automobiles by leveling the grounds between the big name dealerships and independent shops and DIYers, reducing the costs of repairs and giving vehicle owners a choice in where to service their vehicles. 
 
 # ## Data Sources
 # *What 3 data sources have you identified for this project?*
 # *How are you going to relate these datasets?*
 # 📝 <!-- Answer Below -->
 
+# 1. The first data source I am going to be using is the Vehicle Health Index by CarMD. This is going to provide me with information to show the costs of over older VS newer vehicles and their associated repair costs which shows how software dependencies can raise repair prices. I will relate this data source because it provides me with a breakdown in measuring the average annual repair costs among varying categories of repair and shows how the costs of automotive repair have increased over time. 
+# 
+# 
+# 2. The second data source I will use is the Consumer Price Index for All Urban Consumers: Motor Vehicle Maintenance and Repair in U.S. City Average. This is going to show me the cost of vehicle maitenence by year to display the rising costs of automotive repair costs. This will be related by displaying with direct visual information how the costs of vehicle maitenence has increased throughout the years. 
+# 
+# 3. The final data source i will be using is a database provided from kaggle.com, the "Car Parts Price Estimation Dataset." This dataset will provide me with an example cost breakdown of the prices of commmon parts found to repair a Toyota Camry. This will display the increase in cost over a span of a few years. 
+
 # ## Approach and Analysis
 # *What is your approach to answering your project question?*
 # *How will you use the identified data to answer your project question?*
-# 📝 <!-- Start Discussing the project here; you can add as many code cells as you need -->
+# 
+# The approach I have is to combine the cost data, and vehicle data to build a full picture of how repair costs have changed over time. I will demonstrate how repair costs have changed between older cars and newers cars over the last few years. Through the use of repair costs datasets I will measure how repair costs differ by the year and type of repairs performed. This will directly demonstrate how modern vehicles are more expensive to repair as compared to simpler and less complicated older vehicle models. 
 
-# In[1]:
+# In[ ]:
 
 
-# Start your code here
+
 
 
 # ## Resources and References
@@ -41,6 +52,22 @@
 # In[ ]:
 
 
+https://carmd.com/pages/vehicle-health-index
+
+https://fred.stlouisfed.org/series/CUSR0000SETD
+
+https://www.kaggle.com/datasets/shorooq77/car-parts-price-estimation
+
+
+# In[ ]:
+
+
+
+
+
+# In[2]:
+
+
 # ⚠️ Make sure you run this cell at the end of your notebook before every submission!
-get_ipython().system('jupyter nbconvert --to python python-exercises.ipynb')
+get_ipython().system('jupyter nbconvert --to python source.ipynb')
 
