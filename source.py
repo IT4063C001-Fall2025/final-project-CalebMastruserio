83acf2cb47fa8e90ffee2adcb04cb894a9ddc65a
CalebMastruserio <mastrucb@mail.uc.edu>
2025-10-25 18:46:25 -0400

    Completed Checkpoint 2.

--- source.py
+++ source.py
@@ -39,12 +39,6 @@
 # 
 # The approach I have is to combine the cost data, and vehicle data to build a full picture of how repair costs have changed over time. I will demonstrate how repair costs have changed between older cars and newers cars over the last few years. Through the use of repair costs datasets I will measure how repair costs differ by the year and type of repairs performed. This will directly demonstrate how modern vehicles are more expensive to repair as compared to simpler and less complicated older vehicle models. 
 
-# In[ ]:
-
-
-
-
-
 # ## Resources and References
 # *What resources and references have you used for this project?*
 # 📝 <!-- Answer Below -->
@@ -59,13 +53,224 @@ https://fred.stlouisfed.org/series/CUSR0000SETD
 https://www.kaggle.com/datasets/shorooq77/car-parts-price-estimation
 
 
+# ## 1. Exploratory Data Analysis (EDA)
+# You may have (hopefully) found more datasets for your project. If so, you should include them under the "Data Sources" section. If not, that means that so far your data is able to answer your analysis Apply EDA and use Visualizations to answer questions such as:
+# 
+# What insights and interesting information are you able to extract at this stage?
+# Currently, as my question has posed, vehicle repair costs have steadily increased over time. Most espeacially for newer cars with more software and computer dependent systems and features. Electric and diagnostic related fixes are the main driving focus of this trend, while inflation alone does not completely explain the durastic increase overtime in these expenses. These shown trends show that modern vehicles and their overly reliant designs on technology restrict repair access and are contributing to higher overall maintenance costs. 
+# 
+# What are the distributions of my variables?
+# Repair cost and parts prices are both skewed to the right, having most repair bills fall within a moderate price range, and a lower amount containing very high cost electric or software related repairs pulling the average price trend upwards. The CPI data shows a steady upward trend, until you get later into the more recent years, such as the last 5 years, and the prices for maintenance costs have jumped upwards strongly. 
+# 
+# Are there any correlations between my variables?
+# There is a moderate positive correlation between vehicle year and average repair cost, meaning that newer cars tend to cost more to repair and are more expensive to own and operate. This supports the idea that modern cars with complex software and electronics significantly increase repair costs compared to older vehicles with more simpler components, features, and systems. 
+# 
+# What issues can you see in your data at this point?
+# So far I have noticed that some of the datasets contain missing or duplicate values and a couple of the columns and even a few columns are stored as text insteda of numbers. Additionally, there are outliers in part prices and repair bills. Likely to be rare events or even a complex dealer component. This could distort averages and data visualizations. I will go through many of these issues before deeper data analysis can occur. 
+# 
+# Are there any outliers or anomalies? are they relevant to your analysis? or should they be removed?
+# Yes so far in my playing with the data I have seen a few high cost outliers as I mentioned in the previous question. Such as some exsorbant costs that do not reflect a direct trend or indicate a large jump in cost, but could pose a challenge when attempting to provide deeper data analysis. I will do more research in regards to how but it will affect my data and/or visualizations. 
+# 
+# Are there any missing values? how are you going to deal with them?
+# Yes, I have noticed that the Car Price Estimation dataset contains some missing values, such as listing the same parts for specific model years. Those rows will be removed and since they represent only a small amount of the data and are very unlikely to affect the overall trend of data. This will allow me to conduct a linear analysis of the parts costs by year. 
+# 
+# Are there any duplicate values? how are you going to deal with them?
+# Yes, as I have played around with the data, I have noticed as well as in the Car Price Estimation dataset that it does contain dupelicate records for the same exact part and year. Similar to the missing values, I can remove them and still have a clear visual of the averages ensuring they are not skewed. 
+# 
+# 
+# Are there any data types that need to be changed?
+# Yes, only a few columns contain prices in both the CarMD and the Car Prices Estimate datasets are stored with strings as "$" symbols. I will go ahead and convert them to numerical values so that I can conduct more accurate and reflective calculations and visualizations. 
+# 
+# 
+# ## Data Visualization
+# You should use several visualizations to help answer the questions above.
+# 
+# You should have at least 4 visualizations in your notebook, to represent different aspects and valuable insights of your data.
+# 
+# You can use 2 visualization library that you want.
+# 
+# You can use any type of visualization that best represents your data.
+# 
+# You'll need to provide a short description of each visualization and explain what it represents, and what insights you can extract from it.
+# 
+
 # In[ ]:
 
 
+import pandas as pd
+
+df = pd.read_csv(r"c:\Users\kid0001\Documents\University of Cincinnati\Fall Semester 2025\DATA TECH ANALYTICS\Final Project Stuff\2022CarMD.csv")
+
+print(df.head())
+print(df.info())
 
 
+# In[5]:
 
-# In[2]:
+
+df.columns = df.columns.str.replace(" ", "_").str.replace(r"[^\w]", "", regex=True)
+print(df.columns)
+
+
+# In[8]:
+
+
+numeric_cols = ['AvgLabor_Cost', 'Avg_Parts_Cost', 'Avg_Repair_Costs_Parts__Labor']
+
+for col in numeric_cols:
+    df[col] = df[col].replace('[\$,]', '', regex=True).astype(float)
+
+print(df.head())
+
+
+# In[ ]:
+
+
+# Missing value
+print("Missing values:\n", df.isnull().sum())
+
+# dupelicate value
+print("Duplicate rows:", df.duplicated().sum())
+
+
+# In[ ]:
+
+
+## Visual 1: Histogram – Distribution of Total Repair Costs
+# This is what have used to show the typical range VS higher cost outliers. 
+
+plt.figure(figsize=(8,5))
+sns.histplot(df['Avg_Repair_Costs_Parts__Labor'], bins=15, kde=True, color='skyblue')
+plt.title("Distribution of Average Repair Costs Across States – 2022")
+plt.xlabel("Average Repair Cost ($)")
+plt.ylabel("Number of States")
+plt.show()
+
+
+# In[11]:
+
+
+## Visual 2: Boxplot – Identify Outliers
+# This is part of what I have used to display some of the potential outliers for my datasets. 
+# Showing my mean, quartiles, and any extremely outliers.
+
+plt.figure(figsize=(8,5))
+sns.boxplot(y=df['Avg_Repair_Costs_Parts__Labor'], color='lightgreen')
+plt.title("Boxplot of Average Repair Costs – 2022")
+plt.ylabel("Average Repair Cost ($)")
+plt.show()
+
+
+# In[ ]:
+
+
+import pandas as pd
+
+car_parts = pd.read_csv(r"C:\Users\kid0001\Documents\University of Cincinnati\Fall Semester 2025\DATA TECH ANALYTICS\Final Project Stuff\CPE.csv", encoding='latin-1')
+
+print(car_parts.head())
+print(car_parts.info())
+
+
+# In[ ]:
+
+
+# Cleaning column names
+car_parts.columns = car_parts.columns.str.replace(" ", "_").str.replace(r"[^\w]", "", regex=True)
+print("Cleaned columns:", car_parts.columns)
+
+
+# In[ ]:
+
+
+#More cleanup
+import numpy as np
+
+price_cols = ['shop1_eqbal', 'shop2haraj', 'shop3_']
+
+for col in price_cols:
+
+    car_parts[col] = pd.to_numeric(car_parts[col].replace('[\$,]', '', regex=True), errors='coerce')
+
+
+print(car_parts[price_cols].head())
+
+
+# In[ ]:
+
+
+import numpy as np
+
+price_cols = ['shop1_eqbal', 'shop2haraj', 'shop3_']
+for col in price_cols:
+    car_parts[col] = pd.to_numeric(car_parts[col].replace('[\$,]', '', regex=True), errors='coerce')
+
+car_parts['avg_price'] = car_parts[price_cols].mean(axis=1)
+
+print(car_parts[['body_part', 'avg_price']].head())
+
+
+# In[ ]:
+
+
+## Visual 3: CPE bar chart.
+# This bar chart shows that some car parts are a lot more expensive across the shops, supporting the idea that more modern software dependent vehicle repairs increase the repair costs. 
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+plt.figure(figsize=(12,6))
+sns.barplot(data=car_parts, x='body_part', y='avg_price', palette="Blues_r")
+plt.xticks(rotation=45)
+plt.xlabel("Car Part")
+plt.ylabel("Average Price ($)")
+plt.title("Average Price of Car Parts Across Shops")
+plt.show()
+
+
+# In[ ]:
+
+
+## Visual 4: Line Chart of Price comparisons
+# This chart shows the prices for the same parts and how they vary significantly accross the shops displaying the fact of how propriertory systems drive up repair costs. 
+plt.figure(figsize=(12,6))
+for col in price_cols:
+    sns.lineplot(data=car_parts, x='body_part', y=col, marker='o', label=col)
+
+plt.xticks(rotation=45)
+plt.xlabel("Car Part")
+plt.ylabel("Price ($)")
+plt.title("Comparison of Part Prices Across Different Shops")
+plt.legend(title='Shop')
+plt.grid(True)
+plt.show()
+
+
+# ## 2. Data Cleaning and Transformation
+# In this section, you'll clean data per your findings in the EDA section. You will be handling issues such as:
+# 
+# Missing values
+# 
+# Duplicate values
+# 
+# Anomalies and Outliers
+# 
+# Data types transformation.
+# 
+# 
+# You will need to describe the data cleaning process you went through to prepare your data for analysis. This includes your code and the reasoning behind your decisions.
+# 
+# See above the code I used to detect for any missing, dupelicates, outliers, and data types for transformations. I started by cleaning up the data by converting non-numeric values to NAN, dropping any uncecessary columns, transforming price columns to numeric types, creating an average price column, and keeping high cost outliers because they were relevant to showing how the more modern build a vehicle is has a strong positive correllation to increased part and labor bills. 
+# 
+
+# ## 3. Prior Feedback and Updates
+# What feedback did you receive from your peers and/or the teaching team?
+# 
+# What changes have you made to your project based on this feedback?
+# 
+# I did not recieve too much feedback from my peers, I just spent this time on refining my data cleansing, and making sure that my datasets were ready for analysis and practicing how I can use the data to make visualizations to support my findings on my project question. 
+
+# In[1]:
 
 
 # ⚠️ Make sure you run this cell at the end of your notebook before every submission!
